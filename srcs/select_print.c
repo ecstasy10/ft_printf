e745584ece589c4b07a1bca902efbe80c39c7574
David Balboa rojo <dbalboa-@c2r18s5.42madrid.com>
2020-01-28 23:27:54 +0100

    args

--- srcs/select_print.c
+++ srcs/select_print.c
@@ -6,7 +6,7 @@
 /*   By: dbalboa- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/28 21:13:13 by dbalboa-          #+#    #+#             */
-/*   Updated: 2020/01/28 23:07:49 by dbalboa-         ###   ########.fr       */
+/*   Updated: 2020/01/28 23:27:50 by dbalboa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,6 @@ t_tab		*select_print(t_tab *tab)
 		tab->flag_ident = '%';
 		print_c(tab);
 	}
+    //va_arg(tab->args, int)
 	return (tab);
 }
